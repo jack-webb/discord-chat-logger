1157f1643ba3cfec0fa931d6de8ffa8f32d4c496
Jack Webb <git@jackwebb.me>
2021-04-23 12:11:17 +0100

    Fix doc

--- config.example.py
+++ config.example.py
@@ -1,6 +1,6 @@
 token = ""  # Your bot token
 prefix = ""  # Prefix for bot commands
-log_channel = ""  # The ID of the channel the bot listens to for log requests
+log_channel = 0  # The ID of the channel the bot listens to for log requests (No quotes)
 description = "Logging chat messages"  # A description of the bot
 database = {
     "name": "",  # POSTGRES_DB
